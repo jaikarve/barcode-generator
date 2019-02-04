4edb8f056d83934ab4fb7ce34ebc74a1945d3c39
Jai Karve 2 <jaikarve@MCWFHJKARVE.local>
2019-02-04 10:26:42 -0800

    fixed untracked files

--- node_modules/kinvey-datalink-errors/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "extends": "kinvey-platform",
-  "rules" : {
-    // This allows properties of parameters to be written but not parameters themselves
-    "no-param-reassign": ["error", { "props": false }],
-  }
-}
